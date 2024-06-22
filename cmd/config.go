4743689a7e6e5ce47c570acc088095ee4d43235d
Christian Neff <christian.neff@gmail.com>
2024-06-22 18:30:45 +0200

    feat: Add "config" and "config get" commands

---
+++ cmd/config.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"github.com/seabreeze-project/seabreeze/util"
+	"github.com/spf13/cobra"
+)
+
+// configCmd represents the 'config' command
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: "Manage the configuration",
+	Long: `This command group allows you to manage the configuration.
+Calling the "config" command without a subcommand will print information about the loaded configuration.`,
+	Args: cobra.NoArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		h := util.NewHelper(cmd)
+
+		table := h.Table()
+		table.Wrap = true
+		table.AddRow("Config file used:", Core.Config().SourceFile())
+		h.Println(table)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(configCmd)
+}
---
+++ cmd/config_get.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"github.com/seabreeze-project/seabreeze/util"
+	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v3"
+)
+
+// configGetCmd represents the 'config get' command
+var configGetCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Get a configuration value",
+	Long:  `This command returns the value of the given configuration key.`,
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		h := util.NewHelper(cmd)
+
+		val := Core.Config().Get(args[0])
+		if val == nil {
+			h.Fatalf("Key %q undefined\n", args[0])
+		}
+		out, err := yaml.Marshal(val)
+		if err != nil {
+			h.Fatal(err)
+		}
+		h.Println(string(out))
+	},
+}
+
+func init() {
+	configCmd.AddCommand(configGetCmd)
+}
